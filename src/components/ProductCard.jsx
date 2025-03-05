28d4a6ab8bec6d42729b7376b9d62b7592978fdd
Abhishek Rajput <168184952+abhiayasya@users.noreply.github.com>
2025-03-05 10:55:38 +0530

    update base url

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,7 +8,7 @@ const ProductCard = ({ product }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg p-4 border border-gray-200 hover:shadow-xl transition">
       <img
-        src={`https://38.242.148.71:9084/media/catalog/product/${imageUrl}`}
+        src={`http://38.242.148.71:9084/media/catalog/product/${imageUrl}`}
         alt={product.name} // Use 'name' instead of 'title'
         className="w-full h-60 object-contain"
       />
